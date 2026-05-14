0fb706d58900629b182d5c6624975bdecbffb218
Matteo Baglini <matteo.baglini@gmail.com>
2026-05-14 10:56:06 +0200

    chore(ci): use correct file path

--- release.config.mjs
+++ release.config.mjs
@@ -69,13 +69,13 @@ export default {
     [
       "@semantic-release/exec",
       {
-        "prepareCmd": "sed -i 's|<revision>.*</revision>|<revision>${nextRelease.version}</revision>|' pom.xml && sed -i 's/^pkgver=.*/pkgver=\"${nextRelease.version}\"/' package/PKGBUILD"
+        "prepareCmd": "sed -i 's/^pkgver=.*/pkgver=\"${nextRelease.version}\"/' package/timezone-data/PKGBUILD"
       }
     ],
     [
       '@semantic-release/git',
       {
-        assets: ['pom.xml', 'package/PKGBUILD'],
+        assets: ['package/timezone-data/PKGBUILD'],
         message: 'chore(release): ${nextRelease.version} [skip ci]'
       }
     ],
